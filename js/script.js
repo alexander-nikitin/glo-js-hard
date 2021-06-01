c16b9a1e7c738d406e0dd0a8e2b6703ff603ca6d
Александр Никитин <walter@MacBook-Air-Aleksandr.local>
2021-06-01 22:03:47 +0600

    Готово усложненное домашнее задание урока №4

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 let str;
 
-str = 10;
+str = '       Какая-то очень длинная строка, которую нужно очень долго читать, такое ощущение будто она никогда не заканчивается     ';
 
 function getString(a) {
-  if (tyeof(a) != 'string') {
+  if (typeof(a) != 'string') {
     console.log('Аргумент не является строкой');
   } else {
-    a.trim();
+    a = a.trim()
 
     if (a.length > 30) {
-      
-      console.log(a + '...');
+      console.log(a.substring(0, 30) + '...');
+    } else {
+      console.log(a);
     }
     
   }
 }
 
+getString(str);
